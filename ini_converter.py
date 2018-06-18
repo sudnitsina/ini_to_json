c88010f040938ffdde860132c9957c5c86f6b150
Anna <sudnitsina@gmail.com>
2018-06-18 20:08:53 +0300

    PEP 8

--- ini_converter.py
+++ ini_converter.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python
-import re
-import json
-import shlex
 import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", help="Path to the file")
-args = parser.parse_args()
-
-with open(args.file) as ini:
-    f = ini.read()
+import json
+import re
+import shlex
 
 
 def serializer(data):
-    _json = {"_meta": {"hostvars":{}}}
-    parser = re.compile(
+    if not isinstance(data, str):
+        raise ValueError("Invalid data")
+    _json = {"_meta": {"hostvars": {}}}
+    _parser = re.compile(
                 r'''^\[
                         ([^:\]\s]+)             # group name (see groupname below)
                         (?::(\w+))?             # optional : and tag name
@@ -31,7 +26,7 @@ def serializer(data):
         row = row.strip()
         if row == '' or row.startswith(";") or row.startswith("#"):
             continue
-        m = parser.match(row)
+        m = _parser.match(row)
         if m:
             (groupname, state) = m.groups()        
             state = state or 'hosts'
@@ -39,7 +34,7 @@ def serializer(data):
                 _json[groupname] = {}
             if state not in ['hosts', 'children', 'vars']:
                 title = ":".join(m.groups())
-                #self._raise_error("Section [%s] has unknown type: %s" % (title, state))
+                raise ValueError("Section [%s] has unknown type: %s" % (title, state))
             if state == 'hosts':
                 _json[groupname]["hosts"] = []
             if state == 'vars':
@@ -58,11 +53,20 @@ def serializer(data):
                 _json[groupname][state].append(host)
                 if row.find(" ") != -1:
                     _json["_meta"]["hostvars"][host] = {}
-                    for i in range(1,len(a)):
+                    for i in range(1, len(a)):
                         (var, val) = a[i].split("=")
                         _json["_meta"]["hostvars"][host][var] = val
+            elif state == '':
+                raise ValueError("Invalid data")
     return _json
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help="Path to the file")
+    args = parser.parse_args()
+
+    with open(args.file) as ini:
+        f = ini.read()
+
     print(json.dumps(serializer(f), sort_keys=True, indent=2))
---
+++ tests/tests.py
@@ -0,0 +1,84 @@
+from unittest import TestCase
+
+from ini_to_json.ini_converter import serializer
+
+
+class TestConversion(TestCase):
+    def test_convert(self):
+        test_data = '''
+                    [atlanta]
+                    host1 123="45632" 567=890 #Comment
+                    host2 a="b"
+                    
+                        # ddddd
+                    [raleigh]
+                    host4 ansible_ssh_port=2233
+                    host3
+                    
+                    [southeast:children]
+                    atlanta
+                    raleigh
+                    
+                    [southeast:vars]
+                    some_server="foo.southeast.example.com" # comment
+                    halon_system_timeout=30
+                    self_destruct_countdown=60
+                    escape_pods=2
+                    
+                    [usa:children]
+                    southeast
+                    northeast
+                    
+                    '''
+        result = {'_meta': {'hostvars': {'host1': {'123': '45632', '567': '890'},
+                                         'host2': {'a': 'b'},
+                                         'host4': {'ansible_ssh_port': '2233'}}},
+                  'atlanta': {'hosts': ['host1', 'host2']},
+                  'raleigh': {'hosts': ['host4', 'host3']},
+                  'southeast': {'children': ['atlanta', 'raleigh'],
+                                'vars': {'escape_pods': '2',
+                                         'halon_system_timeout': '30',
+                                         'self_destruct_countdown': '60',
+                                         'some_server': 'foo.southeast.example.com'}},
+                  'usa': {'children': ['southeast', 'northeast']}}
+        my_json = serializer(test_data)
+        self.assertDictEqual(my_json, result)
+
+    def test_wrong_title(self):
+        test_data = '''
+                    [atlanta]
+                    host1 123="45632" 567=890 #Comment
+                    host2 a="b"
+
+                        # ddddd
+                    [raleigh]
+                    host4 ansible_ssh_port=2233
+                    host3
+
+                    [southeast:c]
+                    atlanta
+                    raleigh
+
+                    [southeast:vars]
+                    some_server="foo.southeast.example.com" # comment
+                    halon_system_timeout=30
+                    self_destruct_countdown=60
+                    escape_pods=2
+
+                    [usa:children]
+                    southeast
+                    northeast
+
+                    '''
+        self.assertRaises(ValueError, serializer, test_data)
+
+    def test_wrong_data(self):
+        test_data = 123
+        self.assertRaises(ValueError, serializer, test_data)
+        test_data = '123'
+        self.assertRaises(ValueError, serializer, test_data)
+        test_data = '''
+        123
+        321
+        '''
+        self.assertRaises(ValueError, serializer, test_data)
