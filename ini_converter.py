04e246eb1324d7a80d3305bd058da2ae985699b1
anya <sudnitsina@gmail.com>
2019-04-30 23:23:20 +0300

    Fix data types

--- ini_converter.py
+++ ini_converter.py
@@ -6,56 +6,67 @@ import shlex
 
 
 def serializer(data):
-    """ Convert ini format inventory into json
-    """
+    """ Convert ini format inventory into dict."""
     if not isinstance(data, str):
         raise ValueError("Invalid data")
 
     _json = {"_meta": {"hostvars": {}}}
     _parser = re.compile(
-                r'''^\[
+        r"""^\[
                         ([^:\]\s]+)             # group name
                         (?::(\w+))?             # optional : and tag name
                     \]
                     \s*                         # ignore trailing whitespace
                     (?:\#.*)?                   # and/or a comment till the
                     $                           # end of the line
-                ''', re.X
+                """,
+        re.X,
     )
 
-    state = 'hosts'
-    groupname = 'ungrouped'
+    section = "hosts"
+    groupname = "ungrouped"
     for row in data.splitlines():
         row = row.strip()
-        if row == '' or row.startswith(";") or row.startswith("#"):
+        if row == "" or row.startswith(";") or row.startswith("#"):
             continue
         m = _parser.match(row)
         if m:
-            (groupname, state) = m.groups()
-            state = state or 'hosts'
+            (groupname, section) = m.groups()
+            section = section or "hosts"
             if groupname not in _json:
                 _json[groupname] = {}
         else:
             a = shlex.split(row, comments=True)
-            if state == 'children':
-                _json[groupname].setdefault(state, []).append(a[0])
-            elif state == 'vars':
-                _json[groupname].setdefault(state, {})
-                (var, val) = a[0].split("=")
-                _json[groupname][state][var] = val
-            elif state == 'hosts':
+            if section == "children":
+                _json[groupname].setdefault(section, []).append(a[0])
+            elif section == "vars":
+                _json[groupname].setdefault(section, {})
+                (var, val) = _variable_handler(a[0])
+                _json[groupname][section][var] = val
+            elif section == "hosts":
                 if groupname not in _json:
                     _json[groupname] = {}
                 host = a[0]
-                _json[groupname].setdefault(state, []).append(host)
+                _json[groupname].setdefault(section, []).append(host)
                 if row.find(" ") != -1:
                     _json["_meta"]["hostvars"].setdefault(host, {})
                     for i in range(1, len(a)):
-                        (var, val) = a[i].split("=")
+                        (var, val) = _variable_handler(a[i])
                         _json["_meta"]["hostvars"][host][var] = val
             else:
-                raise ValueError('invalid data: Section {} has unknown type:'.format(state))
+                raise ValueError("Section {} has unknown type".format(section))
     return _json
+
+
+def _variable_handler(string):
+    var, val = string.split("=")
+    try:
+        val = eval(val)
+    except SyntaxError:
+        pass
+    except NameError:
+        pass
+    return var, val
 
 
 if __name__ == "__main__":
